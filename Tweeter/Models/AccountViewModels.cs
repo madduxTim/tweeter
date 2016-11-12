58c3c5ff81949713d87eec5de3f54b6603e5d73a
Tim Maddux <timothymaddux@gmail.com>
2016-11-12 11:05:49 -0600

    Redid homework 1

--- Tweeter/Models/AccountViewModels.cs
+++ Tweeter/Models/AccountViewModels.cs
@@ -49,6 +49,10 @@ namespace Tweeter.Models
     public class LoginViewModel
     {
         [Required]
+        [Display(Name ="UserName")]
+        public string UserName { get; set; }
+
+        [Required]
         [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
@@ -64,6 +68,10 @@ namespace Tweeter.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
